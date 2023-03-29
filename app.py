690f352c91ca7d2d746bdc2b540b6fd9f7b7ab43
Drew Anello <drew.anello@gmail.com>
2023-03-29 14:34:52 -0400

    imports data from .env

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+import os
 from flask import Flask, request
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+import datetime
+from dotenv import load_dotenv
 
 
 app = Flask('conf_scanner')
+
+load_dotenv()
+
+sheet_id = os.environ.get('SHEET_ID')
+
+
+creds = service_account.Credentials.from_service_account_file(
+    'path/to/key.json')
